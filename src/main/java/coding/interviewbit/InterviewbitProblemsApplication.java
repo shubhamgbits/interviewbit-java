64704fe9750b12bb5f11f7644e91c95524ed2f23
Shubham Gupta <shubham@Shubhams-MacBook-Pro.local>
2024-07-16 22:21:26 +0530

    init

---
+++ src/main/java/coding/interviewbit/InterviewbitProblemsApplication.java
@@ -0,0 +1,13 @@
+package coding.interviewbit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewbitProblemsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterviewbitProblemsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/coding/interviewbit/InterviewbitProblemsApplicationTests.java
@@ -0,0 +1,13 @@
+package coding.interviewbit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewbitProblemsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
